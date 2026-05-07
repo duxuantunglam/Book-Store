248277b0f47e1b0b2913e06a87db61879d6b00eb
duxuantunglam <duxuantunglam2k2@gmail.com>
2026-05-07 22:16:58 +0700

    config update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from '@/layout.tsx'
+import HomePage from 'pages/client/home';
 import BookPage from 'pages/client/book.tsx';
 import AboutPage from 'pages/client/about.tsx';
 import LoginPage from 'pages/client/auth/login.tsx';
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
       {
         path: "/book",
         element: <BookPage />,
