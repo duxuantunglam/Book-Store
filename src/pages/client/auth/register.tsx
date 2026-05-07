7c18866a41ad1009417f36d5be9b689674ffe32d
duxuantunglam <duxuantunglam2k2@gmail.com>
2026-05-08 01:39:24 +0700

    register UI

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,9 +1,93 @@
+import React from 'react';
+import { Button, Divider, Form, Input } from 'antd';
+import type { FormProps } from 'antd';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './register.scss';
+
+type FieldType = {
+    fullname?: string;
+    email?: string;
+    password?: string;
+    phone?: string;
+};
+
 const RegisterPage = () => {
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
-            <h1>Register Page</h1>
+        <div className="register-page">
+            <main className='main'>
+                <div className='container'>
+                    <section className='wrapped'>
+                        <div className='heading'>
+                            <h2 className='text text-large'>Đăng ký tài khoản</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="form-register"
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Họ tên"
+                                name="fullname"
+                                rules={[{ required: true, message: 'Vui lòng nhập họ tên' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    { required: true, message: 'Vui lòng nhập email' },
+                                    { type: 'email', message: 'Vui lòng nhập đúng định dạng email' }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Vui lòng nhập mật khẩu' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Số điện thoại"
+                                name="phone"
+                                rules={[{ required: true, message: 'Vui lòng nhập số điện thoại' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item label={null}>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng ký
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </section>
+                </div>
+            </main>
         </div>
-    )
+    );
 }
 
 export default RegisterPage
