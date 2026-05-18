11626365925c79c7a1673a90e7faa333c6b1489e
duxuantunglam <duxuantunglam2k2@gmail.com>
2026-05-18 17:58:27 +0700

    API users

--- src/services/api.ts
+++ src/services/api.ts
@@ -31,3 +31,8 @@ export const logoutAPI = () => {
     const urlBackend = "/api/v1/auth/logout";
     return axios.post<IBackendRes<IUser>>(urlBackend);
 }
+
+export const getUsersAPI = () => {
+    const urlBackend = "/api/v1/user?current=1&pageSize=5";
+    return axios.get<IBackendRes<IModelPaginate<IUserTable>>>(urlBackend);
+}
